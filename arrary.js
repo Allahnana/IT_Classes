e25d31a2591f85f8fd8e245745e0fcb45fa66d62
ALLAHNANA <usmanallahnana@gmail.com>
2021-05-26 10:01:49 +0100

    commiting array

---
+++ arrary.js
@@ -0,0 +1,84 @@
+//const array = [1, 'longson', 4.4, 'liman', 'zabson', false];
+// array[1] = 3.5;
+
+// // push o the last end
+// array.push('fauziyya');
+
+// // clear the last one
+// array.pop();
+
+// //clear the first elemnt
+// array.shift();
+
+// // insert infront
+// array.unshift(70);
+
+// const newArray = array.slice(1,2);
+
+// splice
+//array.splice(1,1);
+//console.log(array);
+
+
+// ASSIGNMENT
+// splice //
+// copyWithin //
+// enteries //
+// every //
+// filter //
+// forEach //
+
+// map
+// map a value to its original value
+
+// const numbers = [1, 3, 5, 7, 90];
+// const changedNumbers = numbers.map(function(item){
+//     return item / 2;
+// });
+// console.log(changedNumbers);
+
+// const age = [25,30,35,40,45,50,55,60,63];
+// const newAge = age.map(function(item){
+//     if(item > 30){
+//         return item / 2;
+//     };
+// });
+
+// console.log(newAge);
+
+// const ages = [25,30,35,40,45,50,55,60,63];
+
+// const newAge = ages.map((age, index) => {
+//     if(index == 4){
+//         return age / 3
+//     } else {
+//         return age;
+//     };
+// });
+
+
+
+// const sumTotal = ages.map(function(sum){
+//     return sum * 2;
+// }).reduce((p,n) => {
+//     return p + n;
+// });
+
+// console.log(sumTotal);
+
+// const sumAge = sumTotal.reduce((p,n) =>{
+//     return p + n;
+// });
+
+// console.log(sumAge);
+
+// REDUCING AN ARRAY
+//const totalAges = ages.reduce((p,n) => {
+//     console.log(p,n);
+// }); 
+// console.log(totalAges);
+
+const ABDULHADDI = ['FAUZZIYA', 'RAHMA', 'MARDIYYA', 'MAI JAN IDO'];
+ABDULHADDI.forEach(function(marry,index){
+    console.log(index, marry);
+});      
